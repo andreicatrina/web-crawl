8f3912e37f5767c14f69d7956017afc68ed11c1a
andreicatrina <Catrina.andrei93@yahoo.com>
2023-11-07 17:15:17 +0200

    web crawler v1.1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-// /App.js
+// Import necessary libraries
 import React, { useState } from "react";
 import axios from "axios";
 
 function App() {
+  // State for URL input and scraping result
   const [url, setUrl] = useState("");
-  const [results, setResults] = useState([]);
+  const [result, setResult] = useState({ emailAddresses: null });
 
-  const handleCrawl = async () => {
+  // Function to handle scraping
+  const handleScrape = async () => {
     try {
-      const response = await axios.post("http://localhost:3001/crawl", { url });
-      setResults(response.data.results);
+      // Make a POST request to the backend with the entered URL
+      const response = await axios.post("http://localhost:5000/scrape", { url });
+      console.log("Response from Backend:", response.data);
+
+      // Update state with the scraping result
+      setResult(response.data);
     } catch (error) {
-      console.error(error);
+      console.error("Error:", error.message);
     }
   };
 
+  // Render the UI
   return (
     <div>
-      <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
-      <button onClick={handleCrawl}>Crawl</button>
-
+      <h1>Email Extractor</h1>
+      <label>
+        Enter URL:
+        <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+      </label>
+      <button onClick={handleScrape}>Scrape</button>
       <div>
-        <h2>Results</h2>
-        <pre>{JSON.stringify(results, null, 2)}</pre>
+        <h2>Email Addresses:</h2>
+        <ul>
+          {result.emailAddresses && result.emailAddresses.length > 0
+            ? result.emailAddresses.map((email, index) => <li key={index}>{email}</li>)
+            : "No email addresses found."}
+        </ul>
       </div>
     </div>
   );
--- src/server.js
+++ src/server.js
@@ -1,53 +1,68 @@
-// /server.js
+// Import necessary libraries
 const express = require("express");
-const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const cors = require("cors");
 
+// Create an Express app
+const app = express();
+const port = 5000;
+
+// Use CORS middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.post("/crawl", async (req, res) => {
-  const { url } = req.body;
-
+// Define the /scrape endpoint
+app.post("/scrape", async (req, res) => {
   try {
+    // Extract the URL from the request body
+    const { url } = req.body;
+
+    // Validate the URL
+    if (!url) {
+      throw new Error("URL is required.");
+    }
+
+    // Send a GET request to the provided URL
     const response = await axios.get(url);
+
+    // Load the HTML content into Cheerio
     const $ = cheerio.load(response.data);
 
-    const results = [];
-
-    $("p, div, a").each((index, element) => {
-      const text = $(element).text();
-      const phoneNumbers = extractPhoneNumbers(text);
-
-      const emails = text.match(/\S+@\S+\.\S+/g);
-
-      const extractPhoneNumbers = (text) => {
-        const phoneNumbersRegex = /\b\d+\b/g;
-        const matches = text.match(phoneNumbersRegex) || [];
-
-        const cleanNumbers = matches.map(
-          (number) => number.replace(/\D/g, "") // Remove non-digit characters
-        );
-
-        return cleanNumbers;
-      };
-
-      if (phoneNumbers || emails) {
-        results.push({ phoneNumbers, emails });
+    // Extract emails from href attributes in <a> tags
+    const emailAddressesFromLinks = [];
+    $("a[href]").each((index, element) => {
+      const href = $(element).attr("href");
+      const emailMatch = href.match(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g);
+      if (emailMatch) {
+        emailAddressesFromLinks.push(emailMatch[0]);
       }
     });
 
-    res.json({ success: true, results });
+    // Extract emails from text content in <p> and <div> tags
+    const emailAddressesFromText = [];
+    $("p, div").each((index, element) => {
+      const textContent = $(element).text();
+      const emailMatch = textContent.match(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g);
+      if (emailMatch) {
+        emailAddressesFromText.push(emailMatch[0]);
+      }
+    });
+
+    // Combine the email addresses from all sources
+    const allEmailAddresses = [...emailAddressesFromLinks, ...emailAddressesFromText];
+
+    console.log("Email Addresses:", allEmailAddresses);
+
+    // Send the extracted emails in the response
+    res.json({ emailAddresses: allEmailAddresses });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: "Internal Server Error" });
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ error: error.message });
   }
 });
 
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
